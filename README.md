6ac84ab8ef68b8b92251daee6c29cc7cd31ee8a5
Wilson Rodrigues <w.rodrigues@swordhealth.com>
2022-06-06 20:27:46 +0100

    feat: 🎸 initialize repository

--- README.md
+++ README.md
@@ -2,4 +2,61 @@
 
 ## Description
 
-C++ application to execute local scripts and applications
+C++ application to execute local scripts and/or application. \
+The purpose of this application is to allow to run multiple automations scripts using one single documented application anywhere by CLI and add new scripts without changing the starter application.
+
+## How it works
+
+- **starter** application is installed in */usr/local/bin*
+- configuration folder is created in */home/user/.starter*.
+- *config.yaml* file in *~/.starter/* folder set what commands will be allowed **example:** [config.yaml](./config.yaml.template)
+- scripts and applications to execute must be added to *~/.starter/executables*
+
+## Example
+
+Execute the object with flag *flag-example* configured in *config.yaml* file:
+
+```bash
+    starter -e flag-example
+```
+
+Print config.yaml file content:
+
+```bash
+    starter -s
+```
+
+Print **starter** application help:
+
+```bash
+    starter -h
+```
+
+## config.yaml file
+
+*config.yaml* file define what executables can be xecuted
+
+```yaml
+    - flag: a-script # <- The flag used in "starter -e flag"
+    description: Execute script A
+    exec: "a_script.sh" # <- The script name in ~/.starter/executables/ to be executed
+    ...
+```
+
+## Makefile
+
+Makefile help to build, install and develop the application
+
+```bash
+    # Build satrter application
+    make starter
+
+    # Copy application binary to /uss/local/bin and setup ~/.starter folder
+    make install
+
+    # Run application with example-flag (used for development)
+    make run-exec flag=example-flag
+
+    # Remove application binary
+    make clean
+```
--- src/starter.cc
+++ src/starter.cc
@@ -1,66 +1,114 @@
-#include <iostream>
-
 #include "cxxopts.hh"
+#include <yaml-cpp/yaml.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-void
-parse(int argc, const char* argv[])
-{
-  try
-  {
-    cxxopts::Options options(argv[0], "[ Starter command line options ]\n");
-    options
-      .positional_help("[optional args]")
-      .show_positional_help();
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
-    options
-      .set_width(70)
-      .set_tab_expansion()
-      .allow_unrecognised_options()
-      .add_options()
-      ("e,executable", "Executable to run", cxxopts::value<std::string>())
-      ("help", "Print help")
-    #ifdef CXXOPTS_USE_UNICODE
-      ("unicode", u8"A help option with non-ascii: à. Here the size of the"
-        " string should be correct")
-    #endif
-    ;
+std::string configPath;
+std::string execPath;
 
-    auto result = options.parse(argc, argv);
+struct Action {
+   std::string flag;
+   std::string description;
+   std::string exec;
+};
 
-    if (result.count("help"))
-    {
-      std::cout << options.help({"", "Group"}) << std::endl;
-      exit(0);
-    }
-
-    if (result.count("executable"))
-    {
-      std::cout << "Executable = " << result["executable"].as<std::string>()
-        << std::endl;
-    }
-
-    std::cout << "Arguments remain = " << argc << std::endl;
-
-    auto arguments = result.arguments();
-    std::cout << "Saw " << arguments.size() << " arguments" << std::endl;
-
-    std::cout << "Unmatched options: ";
-    for (const auto& option: result.unmatched())
-    {
-      std::cout << "'" << option << "' ";
-    }
-    std::cout << std::endl;
-  }
-  catch (const cxxopts::OptionException& e)
-  {
-    std::cout << "error parsing options: " << e.what() << std::endl;
-    exit(1);
-  }
+void operator >> (const YAML::Node& node, Action& action) {
+   action.flag = node["flag"].as<std::string>();
+   action.description = node["description"].as<std::string>();
+   action.exec = node["exec"].as<std::string>();
 }
 
-int main(int argc, const char* argv[])
-{
-  parse(argc, argv);
+Action getAction(std::string flag) {
+    YAML::Node config = YAML::LoadFile(configPath);
 
-  return 0;
+    Action result;
+    const unsigned int numActions = config.size();
+    for(unsigned index = 0; index < numActions; index++) {
+        Action action;
+        config[index] >> action;
+        if (flag == action.flag) {
+            result = action;
+            break;
+        }
+    }
+
+    std::ofstream fout(configPath);
+    fout << config;
+
+    return result;
+}
+
+void parse(int argc, const char* argv[]) {
+    try {
+        cxxopts::Options options(argv[0], "Starter command line executer\n");
+        options.positional_help("[optional args]")
+            .show_positional_help();
+
+        options.set_width(70)
+            .set_tab_expansion()
+            .allow_unrecognised_options()
+            .add_options()
+            ("e,executable", "Executable to run", cxxopts::value<std::string>())
+            ("s,show", "Show config options")
+            ("h,help", "Print help")
+        #ifdef CXXOPTS_USE_UNICODE
+        ("unicode", u8"A help option with non-ascii: à. Here the size of the"
+            " string should be correct")
+        #endif
+        ;
+
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help")) {
+            std::cout << options.help({"", "Group"}) << std::endl;
+            exit(0);
+        }
+
+        if (result.count("show")) {
+            std::string cmd = "cat " + configPath;
+            system(cmd.c_str());
+            exit(0);
+        }
+
+        if (result.count("executable")) {
+            std::string flag = result["executable"].as<std::string>();
+            Action action = getAction(flag);
+            if (action.flag.empty()) {
+                std::cout << "No executable found for flag: " << flag << std::endl;
+            } else {
+                std::cout << "Description: " << action.description << std::endl;
+                std::string cmd = execPath + action.exec;
+                system(cmd.c_str());
+            }
+        }
+
+        if (0 == result.arguments().size() || result.unmatched().size() > 0) {
+            std::cout << "Use help flag!" << std::endl;
+        }
+
+    } catch (const cxxopts::OptionException& e) {
+        std::cout << "error parsing options: " << e.what() << std::endl;
+        exit(1);
+    }
+}
+
+void setPaths() {
+    const char *homePath;
+    if ((homePath = getenv("HOME")) == NULL) {
+        homePath = getpwuid(getuid())->pw_dir;
+    }
+    configPath = std::string(homePath) + std::string("/.starter/config.yaml");
+    execPath = std::string(homePath) + std::string("/.starter/executables/");
+}
+
+int main(int argc, const char* argv[]) {
+    setPaths();
+    parse(argc, argv);
+
+    return 0;
 }
